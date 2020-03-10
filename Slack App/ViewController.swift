7310142caa608bfcf57e03f452fad0b29b19b674
mmadney <mohamedmadney1@gmail.com>
2020-03-10 23:53:19 +0200

    Initial Commit

---
+++ Slack App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slack App
+//
+//  Created by Mohamed on 3/10/20.
+//  Copyright © 2020 Qruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
