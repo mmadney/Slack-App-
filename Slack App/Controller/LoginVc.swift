011e0bd77d13309917bee524b20b0b66e8c8fe5e
mmadney <mohamedmadney1@gmail.com>
2020-03-12 19:32:26 +0200

    Add View Login

---
+++ Slack App/Controller/LoginVc.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVc.swift
+//  Slack App
+//
+//  Created by Mohamed on 3/12/20.
+//  Copyright © 2020 Qruz. All rights reserved.
+//
+
+import UIKit
+
+class LoginVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
